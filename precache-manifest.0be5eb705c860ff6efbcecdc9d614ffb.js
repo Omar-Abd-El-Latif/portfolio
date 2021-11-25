c4c89f2fb523e1bc4713d4a21f218b10d470f53f
Omar-Abd-El-Latif <omar299980@yahoo.com>
2021-11-25 11:55:53 +0200

    Updates

---
+++ precache-manifest.0be5eb705c860ff6efbcecdc9d614ffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0fc1bee6cc48d928d2c4d5c708e1e6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1d1c7201741dd9de5e69",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1bc8d8b5203abf0c2a75",
+    "url": "/portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "1d1c7201741dd9de5e69",
+    "url": "/portfolio/static/js/2.c3689884.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.c3689884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc8d8b5203abf0c2a75",
+    "url": "/portfolio/static/js/main.31879e88.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59b2113098c379a40301cfdcea077017.js"
+  "/portfolio/precache-manifest.0be5eb705c860ff6efbcecdc9d614ffb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
